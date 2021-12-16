af3adb4b896a6e2be9c4c4f2a4d0d2d86524cec2
samueladeroun <58010949+samueladeroun@users.noreply.github.com>
2021-12-16 15:42:01 -0500

    Create sec.ps1

---
+++ sec.ps1
@@ -0,0 +1,53 @@
+#Get Secrets from Keyvault
+$secret1 = Get-AzKeyVaultSecret -VaultName "fmz-n-kv-secretrotate-01" -Name "secret1" -AsPlainText
+$secret2 = Get-AzKeyVaultSecret -VaultName "fmz-n-kv-secretrotate-01" -Name "secret2" -AsPlainText
+
+#Authentication to TFC from Powershell
+$tfctoken = "20cBmCkT8K4Bsg.atlasv1.eGbWRDDWNkA4pH9rDlKzLEVYPPZ2kuILXzRdbgpW7vStDIpjSOWzzu7Sz4b4uWeflbY"
+$TfcAuthenticationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($tfctoken)")) }
+
+#Rest APIs to update variable set 1
+$updatevar1 = "https://app.terraform.io/api/v2/varsets/varset-WTNyC9Mxawiw2QWu/relationships/vars/var-vKJtaBxvnnBrakQ8"
+$updatevar2 = "https://app.terraform.io/api/v2/varsets/varset-WTNyC9Mxawiw2QWu/relationships/vars/var-Ho9KvaejS8niqWZ4"
+
+#Request Body
+
+$body1 = @'
+{
+  "data": {
+    "type": "vars",
+    "attributes": {
+      "key": "secret1",
+      "value": $secret, 
+      "description": "",
+      "sensitive": false,
+      "category": "terraform",
+      "hcl": false
+    }
+  }
+}
+'@ 
+
+$body2 = @'
+{
+  "data": {
+    "type": "vars",
+    "attributes": {
+      "key": "secret2",
+      "value": Get-AzKeyVaultSecret -VaultName "fmz-n-kv-secretrotate-01" -Name "secret2" -AsPlainText
+      "description": "",
+      "sensitive": false,
+      "category": "terraform",
+      "hcl": false
+    }
+  }
+}
+'@ 
+	
+
+#Invoke Restapi
+Invoke-RestMethod -Uri $updatevar1 -Body $body1 -Method PATCH -ContentType 'application/vnd.api+json' -headers $headers
+
+#Invoke Restapi
+#Invoke-RestMethod -Uri $updatevar2 -Body $body2 -Method PATCH -ContentType 'application/vnd.api+json' -headers $headers
+
